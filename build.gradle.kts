2edf37e87ffff472859a25491da59e3a604f4815
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2023-03-27 13:09:00 +0200

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,102 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import com.github.jengelman.gradle.plugins.shadow.transformers.ServiceFileTransformer
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+group = "no.nav.syfo"
+version = "1.0.0"
+
+val coroutinesVersion = "1.6.4"
+val jacksonVersion = "2.14.2"
+val ktorVersion = "2.2.4"
+val logbackVersion = "1.4.6"
+val logstashEncoderVersion = "7.3"
+val prometheusVersion = "0.15.0"
+val smCommonVersion = "1.9df1108"
+val kotlinVersion = "1.8.10"
+val junitJupiterVersion = "5.9.2"
+val commonsCodecVersion = "1.13"
+
+tasks.withType<Jar> {
+    manifest.attributes["Main-Class"] = "no.nav.syfo.BootstrapKt"
+}
+
+plugins {
+    id("org.jmailen.kotlinter") version "3.10.0"
+    kotlin("jvm") version "1.8.10"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+}
+
+val githubUser: String by project
+val githubPassword: String by project
+
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://maven.pkg.github.com/navikt/syfosm-common")
+        credentials {
+            username = githubUser
+            password = githubPassword
+        }
+    }
+}
+
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion")
+
+    implementation("io.prometheus:simpleclient_hotspot:$prometheusVersion")
+    implementation("io.prometheus:simpleclient_common:$prometheusVersion")
+
+    implementation("io.ktor:ktor-server-netty:$ktorVersion")
+    implementation("io.ktor:ktor-server-content-negotiation:$ktorVersion")
+    implementation("io.ktor:ktor-server-status-pages:$ktorVersion")
+    implementation("io.ktor:ktor-server-call-id:$ktorVersion")
+    implementation("io.ktor:ktor-client-apache:$ktorVersion")
+    // override transient version 1.11 from io.ktor:ktor-client-apache due to security vulnerability
+    // https://devhub.checkmarx.com/cve-details/Cxeb68d52e-5509/
+    implementation("commons-codec:commons-codec:$commonsCodecVersion")
+    implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-jackson:$ktorVersion")
+
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+    implementation("net.logstash.logback:logstash-logback-encoder:$logstashEncoderVersion")
+
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jacksonVersion")
+
+    testImplementation("io.ktor:ktor-server-test-host:$ktorVersion") {
+        exclude(group = "org.eclipse.jetty") 
+    }
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:$junitJupiterVersion")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
+
+}
+
+tasks {
+
+    create("printVersion") {
+        println(project.version)
+    }
+
+    withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = "17"
+    }
+
+    withType<ShadowJar> {
+        transform(ServiceFileTransformer::class.java) {
+            setPath("META-INF/cxf")
+            include("bus-extensions.txt")
+        }
+    }
+
+    withType<Test> {
+        useJUnitPlatform {}
+        testLogging.showStandardStreams = true
+    }
+
+    "check" {
+        dependsOn("formatKotlin")
+    }
+}
