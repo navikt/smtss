f8f957d40a06dff3fb5b73a8b2b599af78ed705d
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2023-04-14 11:23:26 +0200

    Removed XMLInputFactory.SUPPORT_DTD

--- src/main/kotlin/no/nav/syfo/util/JaxBUtils.kt
+++ src/main/kotlin/no/nav/syfo/util/JaxBUtils.kt
@@ -5,13 +5,10 @@ import java.io.StringWriter
 import javax.xml.bind.JAXBContext
 import javax.xml.bind.Marshaller
 import javax.xml.bind.Unmarshaller
-import javax.xml.stream.XMLInputFactory
 
 val tssSamhandlerdataInputJaxBContext: JAXBContext = JAXBContext.newInstance(XMLTssSamhandlerData::class.java)
 val tssSamhandlerdataInputMarshaller: Marshaller = tssSamhandlerdataInputJaxBContext.createMarshaller()
-val tssSamhandlerdataUnmarshaller: Unmarshaller = tssSamhandlerdataInputJaxBContext.createUnmarshaller().apply {
-    setProperty(XMLInputFactory.SUPPORT_DTD, false);
-}
+val tssSamhandlerdataUnmarshaller: Unmarshaller = tssSamhandlerdataInputJaxBContext.createUnmarshaller()
 
 
 fun Marshaller.toString(input: Any): String = StringWriter().use {
