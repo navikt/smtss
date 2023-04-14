9f1edfa0552e8d54340450b5cb6417eb9722a21f
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2023-04-14 10:37:59 +0200

    Resolving XML external entity in user-controlled data

--- src/main/kotlin/no/nav/syfo/util/JaxBUtils.kt
+++ src/main/kotlin/no/nav/syfo/util/JaxBUtils.kt
@@ -5,10 +5,14 @@ import java.io.StringWriter
 import javax.xml.bind.JAXBContext
 import javax.xml.bind.Marshaller
 import javax.xml.bind.Unmarshaller
+import javax.xml.stream.XMLInputFactory
 
 val tssSamhandlerdataInputJaxBContext: JAXBContext = JAXBContext.newInstance(XMLTssSamhandlerData::class.java)
 val tssSamhandlerdataInputMarshaller: Marshaller = tssSamhandlerdataInputJaxBContext.createMarshaller()
-val tssSamhandlerdataUnmarshaller: Unmarshaller = tssSamhandlerdataInputJaxBContext.createUnmarshaller()
+val tssSamhandlerdataUnmarshaller: Unmarshaller = tssSamhandlerdataInputJaxBContext.createUnmarshaller().apply {
+    setProperty(XMLInputFactory.SUPPORT_DTD, false);
+}
+
 
 fun Marshaller.toString(input: Any): String = StringWriter().use {
     marshal(input, it)
