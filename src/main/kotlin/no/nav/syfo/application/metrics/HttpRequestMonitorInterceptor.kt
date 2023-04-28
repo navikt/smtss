1a29c74bdec14363c16c3357ffb99e846cc1cc41
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2023-04-28 08:24:49 +0200

    Log out path to securelogs

--- src/main/kotlin/no/nav/syfo/application/metrics/HttpRequestMonitorInterceptor.kt
+++ src/main/kotlin/no/nav/syfo/application/metrics/HttpRequestMonitorInterceptor.kt
@@ -3,12 +3,14 @@ package no.nav.syfo.application.metrics
 import io.ktor.server.application.ApplicationCall
 import io.ktor.server.request.path
 import io.ktor.util.pipeline.PipelineContext
+import no.nav.syfo.securelog
 
 val REGEX_FNR = """[0-9]{11}""".toRegex()
 
 fun monitorHttpRequests(): suspend PipelineContext<Unit, ApplicationCall>.(Unit) -> Unit {
     return {
         val path = context.request.path()
+        securelog.info("path is $path")
         val label = REGEX_FNR.replace(path, ":samhandlerFnr")
         val timer = HTTP_HISTOGRAM.labels(label).startTimer()
         proceed()
