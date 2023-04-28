598fd5126a33c5e648f11401a10453266eed75ce
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2023-04-28 10:30:46 +0200

    Use path as label

--- src/main/kotlin/no/nav/syfo/application/metrics/HttpRequestMonitorInterceptor.kt
+++ src/main/kotlin/no/nav/syfo/application/metrics/HttpRequestMonitorInterceptor.kt
@@ -5,13 +5,9 @@ import io.ktor.server.request.path
 import io.ktor.util.pipeline.PipelineContext
 import no.nav.syfo.securelog
 
-val REGEX_FNR = """[0-9]{11}""".toRegex()
-
 fun monitorHttpRequests(): suspend PipelineContext<Unit, ApplicationCall>.(Unit) -> Unit {
     return {
-        val path = context.request.path()
-        securelog.info("path is $path")
-        val label = REGEX_FNR.replace(path, ":samhandlerFnr")
+        val label = context.request.path()
         val timer = HTTP_HISTOGRAM.labels(label).startTimer()
         proceed()
         timer.observeDuration()
