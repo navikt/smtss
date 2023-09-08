9c524983b5a0b35fe798d2c473978840c9969ba3
Joakim Taule Kartveit <joakim.taule.kartveit@nav.no>
2023-09-08 10:39:54 +0200

    Remove org.cyclonedx.bom plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,6 @@ plugins {
     id("application")
     kotlin("jvm") version "1.9.0"
     id("com.github.johnrengelman.shadow") version "8.1.1"
-    id("org.cyclonedx.bom") version "1.7.4"
     id("com.diffplug.spotless") version "6.20.0"
 }
 
